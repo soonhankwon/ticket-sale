3266dece55e5f734ce659a4140600cf30ebc237f
soonable@naver.com <soonable@naver.com>
2023-03-13 16:26:07 +0900

    Initial commit

---
+++ src/test/java/com/example/ticketsale/TicketSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketSaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
